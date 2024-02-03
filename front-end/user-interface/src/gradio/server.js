84f7847e83cb0651345ebfd583eddf47e1cc23dd
Connor <connorleung@yahoo.com>
2024-02-03 16:37:21 -0500

    Merge branch 'main' into front-end

--- front-end/user-interface/src/gradio/server.js
+++ front-end/user-interface/src/gradio/server.js
@@ -1,3 +1,3 @@
 import { client } from "@gradio/client";
 
-const app = client("abidlabs/en2fr"); // a Space that translates from English to French
+const app = client("abidlabs/en2fr"); // need to connect it with our own hugging face space

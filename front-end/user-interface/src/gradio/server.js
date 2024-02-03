93b83f9e8636863e4f21474e7d688359b8df18e7
Connor <connorleung@yahoo.com>
2024-02-03 16:35:35 -0500

    front-end merge

---
+++ front-end/user-interface/src/gradio/server.js
@@ -0,0 +1,3 @@
+import { client } from "@gradio/client";
+
+const app = client("abidlabs/en2fr"); // a Space that translates from English to French
--- tracking.py
+++ tracking.py
@@ -3,20 +3,21 @@ from ultralytics import YOLO
 import numpy as np
 from collections import defaultdict
 import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
-video = 'demo1.mp4'
+video = 'videos/demo1.mp4'
 model = YOLO('yolov8n.pt')
 
 cap = cv2.VideoCapture(video)
 history = defaultdict(lambda: [])
-all_tracks = []
 
 while cap.isOpened:
     ret, frame = cap.read()
     if ret:
-    
+
         results = model.track(frame, persist=True)
-        
+
         if results[0].boxes.id is None:
             continue
 
@@ -32,23 +33,54 @@ while cap.isOpened:
                 track.pop(0)
 
             points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-            all_tracks.append(points)
-            cv2.polylines(annotation, [points], isClosed=False, color=(230, 230, 230), thickness=5)
+            cv2.polylines(annotation, [points], isClosed=False, color=(
+                230, 230, 230), thickness=5)
 
         cv2.imshow("Stream: ", annotation)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-    else: # end of video
+    else:  # end of video
         break
 
 cap.release()
 cv2.destroyAllWindows()
 
-if all_tracks:
+if history:
     fig, ax = plt.subplots()
+    # print(history[1])
+    # trajectory = np.array(history[1])
+    # print(trajectory)
+    # ax.plot(trajectory[:, 0], trajectory[:, 1], linewidth=2)
+    numpy_history = []
+    for track_id, points in history.items():
+        points = np.array(points)
+        ax.plot(points[:, 0], points[:, 1], linewidth=2)
+        numpy_history.append(points)
+    # print(numpy_history)
+    x_points = [point[:, 0] for point in numpy_history]
+    y_points = [point[:, 1] for point in numpy_history]
+    print(y_points)
+    print(x_points)
+    plt.savefig('output_plot.png')
 
-    for points in all_tracks:
-        ax.plot(points[:, 0, 0], points[:, 0, 1], color='blue', linewidth=2)
-        plt.savefig('output_plot.png')
-    
+regression = linear_model.LinearRegression()
+car_regressions = []
+for x, y in zip(x_points, y_points):
+    stats = {}
+    x = x.reshape(-1, 1)
+    regression.fit(x, y)
+    y_pred = regression.predict(x)
+    stats['mean_squared_error'] = mean_squared_error(y, y_pred)
+    stats['r2_score'] = r2_score(y, y_pred)
+    stats['x_vals'] = x
+    stats['y_vals'] = y
+    stats['y_pred'] = y_pred
+    car_regressions.append(stats)
+    # print(stats)
+
+fig, ax = plt.subplots()
+car_reg = car_regressions[6]
+print(f"{car_reg['x_vals'].shape}, mean_squared_error: {car_reg['mean_squared_error']}, r2_score: {car_reg['r2_score']}")
+ax.plot(car_reg['x_vals'], car_reg['y_vals'], linewidth=2)
+ax.plot(car_reg['x_vals'], car_reg['y_pred'], linewidth=2)
